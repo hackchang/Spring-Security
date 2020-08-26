1fd25c01c3061f0a7180d7adcfd268fe73e9442c
hackchang <haechang__@naver.com>
2020-08-26 17:36:40 +0900

    security join

---
+++ src/main/java/com/haechang/security1/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.haechang.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터(Security Config)가 스프링 필터체인에 등록
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	// 해당 메소드의 리턴 객체를 IoC로 등록해준다.
+	@Bean
+	public BCryptPasswordEncoder encodePwd() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/user/**").authenticated()
+			.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.anyRequest().permitAll()
+			.and()
+			.formLogin()
+			.loginPage("/loginForm");
+	}
+	
+}
--- src/main/java/com/haechang/security1/controller/IndexController.java
+++ src/main/java/com/haechang/security1/controller/IndexController.java
@@ -1,11 +1,23 @@
 package com.haechang.security1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.haechang.security1.model.User;
+import com.haechang.security1.repository.UserRepository;
 
 @Controller // View를 리턴하겠다.
 public class IndexController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	// 머스테치 기본폴더 src/main/resources/
 	// 뷰리졸버 설정 : templates (prefix), .mustache (suffix)
@@ -31,18 +43,23 @@ public class IndexController {
 	}
 	
 	// 스프링 시큐리티가 가로챔. - SecurityConfig 파일 생성 후 작동안함.
-	@GetMapping("/login")
-	public @ResponseBody String login() {
-		return "login";
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "loginForm";
 	}
 	
-	@GetMapping("/join")
-	public @ResponseBody String join() {
-		return "join";
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		return "joinForm";
 	}
 	
-	@GetMapping("/joinProc")
-	public @ResponseBody String joinProc() {
-		return "회원가입 완료";
+	@PostMapping("/join")
+	public String join(User user) {
+		user.setRole("ROLE_USER");
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		userRepository.save(user); // 비밀번호 암호화해야 로그인 가능.
+		return "redirect:/loginForm";
 	}
+	
+	
 }
