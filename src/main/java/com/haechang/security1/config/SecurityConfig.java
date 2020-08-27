bc0a278c9f1e1fe920b00161e6728d7c3d0a5564
hackchang <haechang__@naver.com>
2020-08-27 16:17:14 +0900

    oauth2 google finish

--- src/main/java/com/haechang/security1/config/SecurityConfig.java
+++ src/main/java/com/haechang/security1/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.haechang.security1.config;
+// 구글 로그인 후 처리 필요함. 
+// kakao - 1. 코드받기(인중) 2. 액세스토큰(권한) 3. 프로필정보 가져옴 4. 자동회원가입
+// 4-2. 정보가 추가적으로 필요할 때 더 추가해야됨.
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -7,6 +11,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.haechang.security1.config.oauth.PrincipalOauth2UserService;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터(Security Config)가 스프링 필터체인에 등록
@@ -19,6 +25,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public BCryptPasswordEncoder encodePwd() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Autowired
+	private PrincipalOauth2UserService principalOauth2UserService;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -35,7 +44,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.defaultSuccessUrl("/")
 			.and()
 			.oauth2Login()
-			.loginPage("/loginForm");
+			.loginPage("/loginForm")
+			.userInfoEndpoint()
+			.userService(principalOauth2UserService); // Tip. 코드 안받고, 액세스 토큰+사용자 프로필정보 받음.
+		
 	}
 	
 }
--- src/main/java/com/haechang/security1/controller/IndexController.java
+++ src/main/java/com/haechang/security1/controller/IndexController.java
@@ -3,12 +3,17 @@ package com.haechang.security1.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.haechang.security1.config.auth.PrincipalDetails;
 import com.haechang.security1.model.User;
 import com.haechang.security1.repository.UserRepository;
 
@@ -21,6 +26,26 @@ public class IndexController {
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+	@GetMapping("/test/login")
+	public @ResponseBody String testLogin(
+			Authentication authentication,
+			@AuthenticationPrincipal PrincipalDetails userDetails) {
+		System.out.println("/test/login =================");
+		PrincipalDetails principalDetails = (PrincipalDetails)authentication.getPrincipal();
+		System.out.println("authentication : "+principalDetails.getUser());
+		System.out.println("userDetails : "+userDetails.getUser());
+		return "세션 정보확인";
+	}
+	
+	@GetMapping("/test/oauth/login")
+	public @ResponseBody String testOAuthLogin(
+			Authentication authentication) {
+		System.out.println("/test/oauth/login =================");
+		OAuth2User oauth2User = (OAuth2User)authentication.getPrincipal();
+		System.out.println("authentication : "+oauth2User.getAttributes());
+		return "OAuth 세션 정보확인";
+	}
+	
 	// 머스테치 기본폴더 src/main/resources/
 	// 뷰리졸버 설정 : templates (prefix), .mustache (suffix)
 	// src/main/resources/templates/index.mustache
@@ -29,8 +54,11 @@ public class IndexController {
 		return "index";
 	}
 	
+	
+	// OAuth 로그인 , 일반 로그인 모두 PrincipalDetails로 받음.
 	@GetMapping("/user")
-	public @ResponseBody String user() {
+	public @ResponseBody String user(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+		System.out.println("principalDetails : "+principalDetails.getUser());
 		return "user";
 	}
 	
